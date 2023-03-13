f72cda1ac3b99cf3c2c6b0398f446c5688a68fd3
Olesia <olesia.malikova@lnu.edu.ua>
2023-03-13 03:04:09 +0200

    Fixed models by g2

--- ConsoleWords/Models/Language.cs
+++ ConsoleWords/Models/Language.cs
@@ -11,18 +11,21 @@ namespace words.Models
         public Guid Id { get; set; }
         public string Title { get; set; }
         public List<Word> Words { get; set; }
+        public List<Group> Groups { get; set; }
 
-        public Language(Guid id, string title, List<Word> words)
+        public Language(Guid id, string title, List<Word> words, List<Group> groups)
         {
             Id = id;
             Title = title;
             Words = words;
+            Groups = groups;
         }
 
         public double getAverageLevel()
         {
             return Words.Sum(x => x.Level) / Words.Count;
         }
+
         public override string ToString()
         {
             return $"{Title} : {getAverageLevel()}";
